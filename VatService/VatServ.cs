b17ac1df7d5ade133cee501f128b8c67ca29ef71
Piotr Markowski <markowskipriv@gmail.com>
2022-04-14 16:11:13 +0200

    Created basic class VatServ with properties and methods

---
+++ VatService/VatServ.cs
@@ -0,0 +1,54 @@
+﻿
+using Vat.Services;
+
+namespace VatServices;
+
+public class VatServ
+{
+    public double VatValue { get; set; }
+    readonly IVatProvider _vatProvider;
+
+    public VatServ()
+    {
+        this.VatValue = 0.23;
+    }
+
+    public VatServ(double vatValue)
+    {
+        this.VatValue = vatValue;
+    }
+
+    public VatServ(IVatProvider vatProvider)
+    {
+        this._vatProvider = vatProvider;
+    }
+
+    public double GrossPriceForDefaultVat(Product product)
+    {
+        return CalculateGrossPrice(product.NetPrice, VatValue);
+    }
+
+    public double GrossPriceForVatProvider(Product product)
+    {
+        VatValue = _vatProvider.VatForType(product.ProductType);
+
+        return CalculateGrossPrice(product.NetPrice, VatValue);
+    }
+
+    public double CalculateGrossPrice(double netPrice, double vatValue)
+    {
+        if (vatValue > 1)
+        {
+            throw new ArgumentOutOfRangeException(nameof(vatValue), "Vat Value is too high! Should be lower than 1.");
+        }
+
+        return netPrice * (1 + vatValue);
+    }
+
+    public double GrossPrice(double netPrice, string productType)
+    {
+        VatValue = _vatProvider.VatForType(productType);
+        return CalculateGrossPrice(netPrice, VatValue);
+    }
+
+}
