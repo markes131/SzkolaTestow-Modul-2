2f9e3ccc30359b67784e44303950484daedff16c
Piotr Markowski <markowskipriv@gmail.com>
2022-04-14 16:12:04 +0200

    Created Inferface for VatProvider - to learn mocking idea

---
+++ VatService/IVatProvider.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vat.Services
+{
+    public interface IVatProvider
+    {
+        double DefaultVat();
+
+        double VatForType(string type);
+    }
+}
